7f6cc9d504de8a1dea1e9fd1e82983cb6e32f8a8
Roberta Fortes <betanickf@gmail.com>
2026-06-15 17:26:41 +0200

    Initial commit: mymoviz part5 backend with /movies route

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/movies', async function(req, res) {
+  const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`);
+  const data = await response.json();
+  res.json({ movies: data.results });
+});
+
+module.exports = router;
