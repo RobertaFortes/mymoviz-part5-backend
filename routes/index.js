fc500fcbbd4c47a9f00d266fb6e6322dd3499220
Roberta Fortes <betanickf@gmail.com>
2026-06-17 19:39:24 +0200

    Use node-fetch explicitly instead of global fetch for Vercel compatibility

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 router.get('/movies', async function(req, res) {
   const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}`);
